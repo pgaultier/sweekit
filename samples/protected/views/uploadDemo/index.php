d478b8513d867b3840680cd6ab86a6173d768979
Philippe Gaultier <pgaultier@sweelix.net>
2012-04-03 15:58:40 +0200

    Adding code

--- samples/protected/views/uploadDemo/index.php
+++ samples/protected/views/uploadDemo/index.php
@@ -18,11 +18,11 @@
 <h2>Sweekit components used</h2>
 
 <ul>
+	<li>SwClientScriptBehavior : automatic script insertion</li>
+	<li>SwRenderBehavior : return data with correct datatype</li>
 	<li>SwUploadAction : upload temporary files</li>
 	<li>SwDeleteAction : delete temporary uploaded files</li>
 	<li>SwFileValidator : validate uploaded file</li>
-	<li>SwClientScriptBehavior : automatic script insertion</li>
-	<li>SwRenderBehavior : return data with correct datatype</li>
 </ul>
 
 <h2>Upload two files</h2>
@@ -41,7 +41,6 @@
 				'auto' => true,
 				'ui' => true,
 				'maxFileSize' => '512mb',
-				'multiSelection' => true,
 			),
 			'events'=>array(
 				'beforeUpload' => 'js:function(up, file){ 
@@ -64,7 +63,34 @@
 <?php endif; ?>
 <h4>Code</h4>
 
+<?php 
 
-<?php echo highlight_string("By clicking on <?php echo Sweeml::link('this link', Sweeml::raiseOpenShadowboxUrl(array('displayInfo'))); ?> you will display inline content in the shadowbox", true)?>
+$phpCode =<<<EOPHP
+<fieldset>
+	<legend>My simple upload form</legend>
+	<?php echo Sweeml::beginForm();?>
+		<?php echo Sweeml::errorSummary(\$demoFileForm); ?>
+		<?php echo Sweeml::activeAsyncFileUpload(\$demoFileForm, 'file',array(
+			'config' => array(
+				'runtimes' => 'html5, flash',
+				'auto' => true,
+				'ui' => true,
+				'maxFileSize' => '512mb',
+			),
+			'events'=>array(
+				'beforeUpload' => 'js:function(up, file){ 
+					$(\'#submitButton\').attr(\'disabled\', \'disabled\'); 
+				}',
+				'uploadComplete' => 'js:function(up, files){
+					$(\'#submitButton\').removeAttr(\'disabled\'); 
+				}',
+			)
+		));?>
+		<?php echo Sweeml::htmlButton('submit', array('type' => 'submit', 'id' => 'submitButton')); ?>
+	<?php echo Sweeml::endForm();?>
+</fieldset>
+EOPHP;
+
+echo highlight_string($phpCode, true);
 
 
