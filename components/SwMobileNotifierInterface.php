fdf430f7ddf4d6b61f080deaa5f5e80e0baedb2f
Philippe Gaultier <pgaultier@sweelix.net>
2012-12-21 18:49:25 +0000

    Adding basic interface

---
+++ components/SwMobileNotifierInterface.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * SwMobileNotifierInterface.php
+ *
+ * PHP version 5.2+
+ *
+ * @author    Philippe Gaultier <pgaultier@sweelix.net>
+ * @copyright 2010-2012 Sweelix
+ * @license   http://www.sweelix.net/license license
+ * @version   XXX
+ * @link      http://www.sweelix.net
+ * @category  components
+ * @package   sweekit.components
+ */
+
+/**
+ * All added notifiers must implement this interface.
+ *
+ * @author    Philippe Gaultier <pgaultier@sweelix.net>
+ * @copyright 2010-2012 Sweelix
+ * @license   http://www.sweelix.net/license license
+ * @version   XXX
+ * @link      http://www.sweelix.net
+ * @category  components
+ * @package   sweekit.components
+ *
+ */
+interface SwMobileNotifierInterface {
+	/**
+	 * Prepare one or more messages
+	 * @see SwMobileNotifierInterface::prepare()
+	 *
+	 *
+	 * @param mixed $deviceIds  string if one device is the target else an array with the list of all targets
+	 * @param array $payload    an array which contains all the data to send.
+	 * @param array $parameters an array of extended parameters
+	 */
+	public function prepare($deviceId, $payload, $parameters=null);
+	/**
+	 * Send the notifications
+	 *
+	 * @return void
+	 * @since  XXX
+	 */
+	public function notify();
+	/**
+	 * Get status for current queue.
+	 *
+	 * @see SwMobileNotifierInterface::getStatus()
+	 *
+	 * @return array
+	 * @since  XXX
+	 */
+	public function getStatus();
+}
